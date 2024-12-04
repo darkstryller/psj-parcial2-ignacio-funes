466db456088412f867c10d22230aac105abda1d3
darkstryller <nachowing2002@gmail.com>
2024-12-04 19:19:45 -0300

    hotfix

--- psj segundo parcial ignacio funes/Assets/scripts/Service Locator/IDataSaver.cs
+++
@@ -1,7 +0,0 @@
-public interface IDataSaver
-{
-    void SetString(string key, string value);
-    string GetString(string key, string defaultValue = default);
-    void SetInt(string key, int value);
-    int GetInt(string key, int defaultValue = default);
-}
